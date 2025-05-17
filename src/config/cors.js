b1f430a4c4822be648db52a40d1f09b09fb8c839
DeyCasGuerrero <depordey@gmail.com>
2025-05-17 13:27:29 -0500

    Actualizar opciones CORS para permitir orígenes dinámicos desde variables de entorno

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,6 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const allowedOrigins = ['http://localhost:5173'];
+
+if (process.env.HOST_NAME) {
+  allowedOrigins.push(process.env.HOST_NAME);
+}
+
 export const corsOptions = {
-  origin: ['http://localhost:5173', 'https://tudominio.com'],
+  origin: allowedOrigins,
   credentials: true,
 };
-
-
